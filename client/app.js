3698aecb096c12ba8acf02d7d16465da31db8164
Rijwana <parbinrijwana8@gmail.com>
2026-09-06 15:22:30 +0530

    expenses

--- client/app.js
+++ client/app.js
@@ -1,4 +1,3 @@
-
 const expenses = [
     {
         title :"lunch",
@@ -28,12 +27,7 @@ expenses.push({
     date :"2026-08-31",
 });
 
-console.log(expenses);
-
 const titleInput = document.getElementById("title");
-const title = titleInput.value;
-console.log(title);
-
 
 const amountInput = document.getElementById("amount");
 
@@ -42,9 +36,45 @@ const categoryInput = document.getElementById("category");
 const dateInput = document.getElementById("date");
 
 const element = document.getElementById("button");
+
 element.addEventListener("click", function(){
     const title = titleInput.value;
-    const amount = amountInput.value;
+    const amount = Number(amountInput.value);
     const category = categoryInput.value;
     const date = dateInput.value;
+
+    const newExpenses = 
+        {
+            title :title,
+            amount : amount,
+            category :category,
+            date : date,
+        };
+
+    expenses.push(newExpenses);
+    updateTotal(expenses);
 });
+
+function updateTotal(expenses){
+    let total = 0;
+    expenses.forEach(function(expense){
+        total = total + expense.amount;
+    });
+
+    const totalInput = document.getElementById("totalExpenses");
+    totalInput.textContent = total;
+}
+
+function renderExpenses(expenses){
+    const expenseList = document.getElementById("expenseItems");
+    expenses.forEach(function(expense){
+        const ele = document.createElement("li");
+        
+        ele.textContent = `${expense.title}- ₹${expense.amount}- ${expense.category}- ${expense.date}`;
+        expenseList.appendChild(ele);
+    });
+}
+        
+updateTotal(expenses);
+renderExpenses(expenses);
+
